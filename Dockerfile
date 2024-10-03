ace32e7dbcb635f630cfdaf66d54ced3d62aec62
Mehidi Hasan <www.mehidi.me@gmail.com>
2024-10-04 01:53:13 +0600

    update

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,8 @@ ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
     PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
 
 WORKDIR /usr/src/app
-RUN chown -R admin:admin /usr/src/app
-RUN chmod 755 /usr/src/app
-USER admin
 
 COPY package*.json ./
 RUN npm ci
 COPY . .
-
-
-
 CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 7800;
 
-
-app.use("/public", express.static(path.join(__dirname, "public")));
+// Serve files from /tmp directory
+app.use("/files", express.static("/tmp"));
 
 app.get("/download-pdf/:vin/:auth", async (req, res) => {
   const vin = req.params.vin;
@@ -44,14 +44,14 @@ app.get("/download-pdf/:vin/:auth", async (req, res) => {
 
   try {
     const response = await fetch(url, options);
-
     const data = await response.json();
 
     if (!data || !data.vhrHtml) {
       return res.status(400).send("No HTML content found");
     }
 
-    const filePath = path.join(__dirname, "public/html", `${vin}.html`);
+    // Use /tmp directory for storing the HTML file
+    const filePath = path.join("/tmp", `${vin}.html`);
     fs.writeFile(filePath, data.vhrHtml, (err) => {
       if (err) {
         console.error("Error writing file", err);
@@ -59,12 +59,6 @@ app.get("/download-pdf/:vin/:auth", async (req, res) => {
       }
       console.log("HTML file created successfully!");
     });
-
-    // const browser = await puppeteer.launch({
-    //   headless: true,
-    //   defaultViewport: null,
-    //   args: ["--window-size=1920,1080"],
-    // });
 
     const browser = await puppeteer.launch({
       args: [
@@ -78,28 +72,19 @@ app.get("/download-pdf/:vin/:auth", async (req, res) => {
           ? process.env.PUPPETEER_EXECUTABLE_PATH
           : puppeteer.executablePath(),
     });
-    const page = await browser.newPage();
 
-    await page.goto(`${fullUrl}/public/html/${vin}.html`, {
+    const page = await browser.newPage();
+    await page.goto(`${fullUrl}/files/${vin}.html`, {
       waitUntil: "domcontentloaded",
     });
 
-    // await page.waitForSelector('#top-bar > div > div.right-header-bar-items > div > button > span');
-    // await page.click("#top-bar > div > div.right-header-bar-items > div > button > span");
-
-    // await page.waitForSelector(".HistoryBasedValueSection");
-    // await page.click(".HistoryBasedValueSection");
-
-    // await page.waitForSelector("div > div.modal_footer > div > div > button:nth-child(1)");
-    // await page.click(".modal_footer_actions button:first-child");
-
     await page.evaluate(() => {
       const element = document.querySelector(".advantage-dealer-badge-info");
-      console.log(element);
       if (element) {
         element.remove();
       }
     });
+
     await page.evaluate(() => {
       const element = document.querySelector(
         ".print-only-report-provided-by-snackbar"
@@ -108,23 +93,21 @@ app.get("/download-pdf/:vin/:auth", async (req, res) => {
         element.remove();
       }
     });
+
     await page.emulateMediaType("print");
 
-    await page.evaluate(() => {
-      window.print();
-    });
-
-    const pdfBuffer = await page.pdf({
+    const pdfPath = path.join("/tmp", `${vin}.pdf`);
+    await page.pdf({
       format: "A4",
       printBackground: true,
-      path: `public/${vin}.pdf`,
+      path: pdfPath,
     });
 
     await browser.close();
 
     return res
       .status(200)
-      .send({ file_path: `${fullUrl}/public/${vin}.pdf`, status: true });
+      .send({ file_path: `${fullUrl}/files/${vin}.pdf`, status: true });
   } catch (err) {
     console.error("Error generating PDF:", err);
     res.status(500).send(err);
